508093f3de806deff73f849b9aa22d0ae2a6aac0
potakaaa <helbirog@gmail.com>
2025-01-11 21:02:43 +0800

    solving prob-20

---
+++ prob-20/main.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack = []
+        for char in s:
+            if char == ")" and "(" in stack:
+                stack.remove("(")
+            elif char == "]" and "[" in stack:
+                stack.remove("[")
+            elif char == "}" and "{" in stack:
+                stack.remove("{")
+            else:
+                stack.append(char)
+            
+        
+        return True if not stack else False
+                
+s = "([)]"
+test = Solution()
+print(test.isValid(s)) # True
